5d7cbad96e24dfc13ba6e87e8eee2d7dbcf537fb
liululu <liu.lulu@chinaebi.vip>
2022-10-28 15:48:00 +0800

    fix:修改首页

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <div>
+      <HelloWorld msg="xiamudaren"/>
+    </div>
+</template>
+
+<style scoped>
+div {
+width: 100vw;
+height: 100vh;
+background-image: url("./assets/night01.gif");
+background-size: cover;
+background-position: center 0px;
+text-align: center;
+background-repeat:no-repeat;
+background-attachment:fixed;
+
+}
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg">
+       <p1>sdf</p1>
+
+    </div>
+</template>
+
+<style scoped>
+
+.bg {
+    /* background-image: url("../assets/bg.jpg"); */
+    background-color: red;
+}
+
+
+</style>
