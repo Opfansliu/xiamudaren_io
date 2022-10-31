e63b2d28292fdffd2d452f7d3e5a3fe6b01a77a2
liululu <liu.lulu@chinaebi.vip>
2022-10-31 11:00:15 +0800

    fix:test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/xiamudaren/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  rollupOptions: {
+    output: { //静态资源分类打包
+      chunkFileNames: 'static/js/[name]-[hash].js',
+      entryFileNames: 'static/js/[name]-[hash].js',
+      assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+      manualChunks(id) { //静态资源分拆打包
+        if (id.includes('node_modules')) {
+          return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      }
     }
   }
 })
