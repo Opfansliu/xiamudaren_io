be7e03dea3ab92ca079b7309c5a3701d0f8951a5
liululu <liu.lulu@chinaebi.vip>
2022-11-02 16:15:35 +0800

    fix：添加dist

---
+++ dist/assets/Skills.98b6e215.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{b as c,c as r}from"./index.e69032b8.js";const o={};function s(t,n){return c(),r("div",null," skills ")}const a=e(o,[["render",s]]);export{a as default};
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -5,7 +5,7 @@
 
 <template>
     <div>
-       556788890023
+       skills
     </div>
 
 </template>
