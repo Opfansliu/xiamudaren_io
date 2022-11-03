230d0ef63bc63cfc4c0614865b4abc3b4b8f95db
liululu <liu.lulu@chinaebi.vip>
2022-11-03 17:36:55 +0800

    fix:test

---
+++ assets/AboutView.46722067.js
@@ -0,0 +1,1 @@
+import{u as s,o as a,c as n,a as e}from"./index.f29fa90b.js";const _={class:"about"},c=e("h1",null,"This is an about page",-1),p={__name:"AboutView",setup(u){const t=s();function o(){t.push("home")}return(i,r)=>(a(),n("div",_,[c,e("button",{onClick:o},"\u8FD9\u662F\u4E00\u4E2A\u6309\u94AE")]))}};export{p as default};
