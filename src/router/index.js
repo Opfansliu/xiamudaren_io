60bb92354f5f0d67d243857a7f7562b4d42d3dec
liululu <liu.lulu@chinaebi.vip>
2022-11-03 14:19:10 +0800

    fix:test

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // import HomeView from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
