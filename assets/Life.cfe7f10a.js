3ca4655ea7ad5699920a8eb4dff2a560514a08aa
liululu <liu.lulu@chinaebi.vip>
2022-11-04 15:22:40 +0800

    fix：添加两个主页

--- assets/Life.cfe7f10a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c as t,a as c}from"./index.36b5ba49.js";const n={},o=c("h1",null,"life",-1),s=[o];function a(r,l){return _(),t("div",null,s)}const f=e(n,[["render",a]]);export{f as default};
